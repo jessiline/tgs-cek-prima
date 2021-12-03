8508fd4a3a016518548d79f6ce65745cb267ecd2
jessiline <jimanuela@student.ciputra.ac.id>
2021-12-03 10:25:14 +0700

    Add project files.

---
+++ tgs cek prima/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tgs_cek_prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
