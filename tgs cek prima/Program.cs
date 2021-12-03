86888bd2ec5a9198e5eb406be3d442dd0bd60f3b
jessiline <jimanuela@student.ciputra.ac.id>
2021-12-03 10:33:53 +0700

    menambahkan code untuk mengecek angka inputan user prima atau bukan

--- tgs cek prima/Program.cs
+++ tgs cek prima/Program.cs
@@ -6,7 +6,29 @@ namespace tgs_cek_prima
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int tes = 1;
+            while (tes == 1)
+            {
+                Console.Write("Masukkan angka : ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                int cek = 0;
+                for (int i = 1; i <= input; i++)
+                {
+                    if (input % i == 0)
+                    {
+                        cek += 1;
+                    }
+
+                }
+                if (cek == 2)
+                {
+                    Console.WriteLine("angka prima");
+                }
+                else
+                {
+                    Console.WriteLine("angka bukan prima");
+                }
+            }
         }
     }
 }
